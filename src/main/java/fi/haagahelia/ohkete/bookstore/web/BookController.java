8908d82169ce5902040e4e296ad1e4ebb361dd57
Tomi Salin <tomisalin@Tomis-MacBook-Pro.local>
2017-09-05 10:39:42 +0300

    Add thymeleaf dependecy and printing books to /booklist

--- src/main/java/fi/haagahelia/ohkete/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/ohkete/bookstore/web/BookController.java
@@ -4,18 +4,21 @@ package fi.haagahelia.ohkete.bookstore.web;
 import fi.haagahelia.ohkete.bookstore.domain.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
-@ResponseBody
 public class BookController {
 
     @Autowired
     private BookRepository repository;
 
-    @RequestMapping("/index")
-    public String hello() {
-        return "hello";
+    @RequestMapping(value="/booklist", method= RequestMethod.GET)
+    public String allBooks(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
