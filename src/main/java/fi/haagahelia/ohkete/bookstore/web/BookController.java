6c2335058a09ee784b382742f7dc40fb58e8256f
Tomi Salin <tomisalin@Tomis-MacBook-Pro.local>
2017-09-05 12:55:08 +0300

    Add creation of book, delete book and bootstrap

--- src/main/java/fi/haagahelia/ohkete/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/ohkete/bookstore/web/BookController.java
@@ -1,10 +1,12 @@
 package fi.haagahelia.ohkete.bookstore.web;
 
 
+import fi.haagahelia.ohkete.bookstore.domain.Book;
 import fi.haagahelia.ohkete.bookstore.domain.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,5 +22,23 @@ public class BookController {
     public String allBooks(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    @RequestMapping(value = "/addbook")
+    public String addBook(Model model){
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        repository.delete(id);
+        return "redirect:/booklist";
     }
 }
