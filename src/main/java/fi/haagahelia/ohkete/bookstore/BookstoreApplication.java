bb4c5dc35513194c668cba796859f875dc9acfc1
Tomi Salin <tomisalin@Tomis-MacBook-Pro.local>
2017-09-19 10:44:52 +0300

    Add user and admin

--- src/main/java/fi/haagahelia/ohkete/bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/ohkete/bookstore/BookstoreApplication.java
@@ -2,6 +2,8 @@ package fi.haagahelia.ohkete.bookstore;
 
 import fi.haagahelia.ohkete.bookstore.domain.Book;
 import fi.haagahelia.ohkete.bookstore.domain.BookRepository;
+import fi.haagahelia.ohkete.bookstore.domain.User;
+import fi.haagahelia.ohkete.bookstore.domain.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -21,11 +23,19 @@ public class BookstoreApplication {
 
 
 	@Bean
-	public CommandLineRunner book(BookRepository repository) {
+	public CommandLineRunner book(BookRepository repository, UserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of books");
 			repository.save(new Book("Voihan Java", "Ville Peltonen", "12345678-12", 2012));
 			repository.save(new Book("Parasta koskaan", "Jokke Jokunen", "87654321-21", 2013));
+
+
+			User user1 = new User("user", "$2a$10$6zqO3c/k/cp7G6bSg1lv0ebGLXUS3JqUgGI7nIsFvvFYyeenplvK.", "USER");
+			User user2 = new User("admin", "$2a$10$OHRy4hu2WbDU6jR..LtXXuuFfnt1fW4jayHr2YmZepnR8qCy/JobC", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
+
+
 
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
@@ -33,5 +43,7 @@ public class BookstoreApplication {
 			}
 			log.info("");
 		};
+
+
 	}
 }
--- src/main/java/fi/haagahelia/ohkete/bookstore/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/ohkete/bookstore/WebSecurityConfig.java
@@ -6,10 +6,15 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -28,9 +33,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+
+    }
+    /*
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER").and()
-                .withUser("admin").password("password").roles("USER", "ADMIN");
+                .withUser("admin").password("admin").roles("ADMIN");
     }
+    */
 }
